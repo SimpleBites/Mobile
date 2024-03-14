0796cd59742479fdaad2806150411f08f0037437
TGcoolman <90685684+TGcoolman@users.noreply.github.com>
2024-03-14 13:39:51 +0100

    basic files + simple nav
    Added main important files/dirs
    Added simple navigation

--- App/App.js
+++ App/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import React from "react"
+import { StyleSheet, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+
+import AppNavigator from "./utils/navigation/AppNavigator"
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<Text>Testiiiiiing!</Text>
-			<Text>Hallo Jur!</Text>
+			<NavigationContainer>
+				<AppNavigator />
+			</NavigationContainer>
 			<StatusBar style="auto" />
 		</View>
 	)
@@ -14,9 +18,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		flex: 1, // Ensure the container takes up the full height
 		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
 	},
 })
